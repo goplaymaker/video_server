acb1184ca8b51dc37f831c4f0fc3ac5a3552956d
goplaymaker <goplaymaker@email>
2022-04-12 11:15:09 +0800

    [IMP] buildprod.sh

--- buildprod.sh
+++ buildprod.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Build web and other services
-cd D:/learn-area/go-wp/video_server/api/
+cd /code/video_server/api/
 env GOOS=linux go build -o ../bin/api
 
-cd D:/learn-area/go-wp/video_server/scheduler/
+cd /code/video_server/scheduler/
 env GOOS=linux go build -o ../bin/scheduler
 
-cd D:/learn-area/go-wp/video_server/streamserver/
+cd /code/video_server/streamserver/
 env GOOS=linux go build -o ../bin/streamserver
 
-cd D:/learn-area/go-wp/video_server/web/
+cd /code/video_server/web/
 env GOOS=linux go build -o ../bin/web
