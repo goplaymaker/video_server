293cacd4d28a7b2e23cfd3ebb75dd2ab36f115a5
goplaymaker <goplaymaker@email>
2022-04-12 11:23:11 +0800

    [IMP] Linux execute file

--- buildprod.sh
+++ buildprod.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+# 由于我 Linux 环境没有装 Go 环境, 所以我是在 Windows 环境下执行该 shell
 # Build web and other services
-cd /code/video_server/api/
+cd D:/learn-area/go-wp/video_server/api/
 env GOOS=linux go build -o ../bin/api
 
-cd /code/video_server/scheduler/
+cd D:/learn-area/go-wp/video_server/scheduler/
 env GOOS=linux go build -o ../bin/scheduler
 
-cd /code/video_server/streamserver/
+cd D:/learn-area/go-wp/video_server/streamserver/
 env GOOS=linux go build -o ../bin/streamserver
 
-cd /code/video_server/web/
+cd D:/learn-area/go-wp/video_server/web/
 env GOOS=linux go build -o ../bin/web
